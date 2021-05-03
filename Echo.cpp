2158cae4b16a9a8c61a09877bb3970043c3614fd
Marshall-F42 <marshallfarris@nevada.unr.edu>
2021-05-02 22:55:50 -0700

    Echo implementation for mono and stereo wave files. Needs to be tested with .wav.

--- Echo.cpp
+++ Echo.cpp
@@ -2,4 +2,40 @@
 //Date: 5/4/2021
 //Processes sound to create an echo
 
+#include "Echo.h"
+#include <iostream>
+
+Echo::Echo(){
+	std::cout << "in defualt echo cotr" << std::endl;
+}
+
+Echo::Echo(int newDelay){
+	delay = newDelay;
+	std::cout << "in echo param cotr" << std::endl;
+}
+
+Echo::~Echo(){
+	std::cout << "in echo dcotr" << std::endl;
+}
+
+template<typename mono>
+void Echo::processBuffer(mono buffer, int bufferSize){
+	std::cout << "in echo processorBuffer" << std::endl;
+
+	for(int i = 0; i <= bufferSize - delay; i++){
+		buffer[i] = ((buffer[i] + buffer[i+delay]) * 0.5);
+	}
+}
+
+
+
+template<typename stereo>
+void Echo::processBuffer(stereo rightBuffer, stereo leftBuffer,  int bufferSize){
+	std::cout << "in echo processorBuffer" << std::endl;
+
+	for(int i = 0; i <= bufferSize - delay; i++){
+		leftBuffer[i] = ((leftBuffer[i] + leftBuffer[i+delay]) * 0.5);
+		rightBuffer[i] = ((rightBuffer[i] + rightBuffer[i+delay]) * 0.5);
+	}
+}
 
--- Echo.h
+++ Echo.h
@@ -5,6 +5,18 @@
 #ifndef ECHO_H
 #define ECHO_H
 
+class Echo{
+	int delay;
+public:
+		
+	Echo();
+	Echo(int newDelay);
+	virtual ~Echo();
+	template<typename mono>
+	void processBuffer(mono buffer, int bufferSize);
 
+	template<typename stereo>
+	void processBuffer(stereo rightBuffer, stereo leftBuffer,  int bufferSize);
+};
 
 #endif
