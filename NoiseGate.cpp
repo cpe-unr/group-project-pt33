6eaf6690c23e3e691270a17e8d2f775390bb7551
Marshall-F42 <marshallfarris@nevada.unr.edu>
2021-05-02 22:18:28 -0700

    Provided code for NoiseGate to handle mono and stereo files.

--- NoiseGate.cpp
+++ NoiseGate.cpp
@@ -2,4 +2,57 @@
 //Date: 5/4/2021
 //Processes sound to cut off sound under a certain threshold
 
+#include "NoiseGate.h"
+#include <iostream>
 
+NoiseGate::NoiseGate(){
+	std::cout << "In NoiseGate default cotr" << std::endl;
+}
+
+NoiseGate::NoiseGate(int noiseGap) : noiseGap(noiseGap){}
+
+NoiseGate::~NoiseGate(){
+	std::cout << "in NoiseGate dcotr" << std::endl;
+}
+
+template<typename mono>
+void NoiseGate::processBuffer(mono buffer, int bufferSize){
+	
+	int upperLimit = 128 + noiseGap;
+	int lowerLimit = 128 - noiseGap;
+//	cout << noiseGap << endl;
+	for(int i = 0; i <= bufferSize; i++){
+		if((buffer[i] <= upperLimit) && (buffer[i] >= lowerLimit)){
+//			cout << "in limit for loop when if statement is true" << endl;
+			buffer[i] = 128;
+		}
+		
+		else{
+//			cout << "in limit for loop when if is false" << endl;
+			buffer[i] = buffer[i];
+		}
+	}
+	
+	std::cout << "in NoiseGate processBuffer" << std::endl;
+}
+
+template<typename stereo>
+void NoiseGate::processBuffer(stereo rightBuffer, stereo leftBuffer, int bufferSize){
+	
+	int upperLimit = noiseGap;
+	int lowerLimit = 0 - noiseGap;
+//	cout << noiseGap << endl;
+	for(int i = 0; i <= bufferSize; i++){
+		if((leftBuffer[i] <= upperLimit) && (leftBuffer[i] >= lowerLimit)){
+//			cout << "in limit for loop when if statement is true" << endl;
+			leftBuffer[i] = 0;
+		}
+		
+		if((rightBuffer[i] <= upperLimit) && (rightBuffer[i] >= lowerLimit)){
+//			cout << "in limit for loop when if statement is true" << endl;
+			rightBuffer[i] = 0;
+		}
+	}
+	
+	std::cout << "in NoiseGate processBuffer" << std::endl;
+}
--- NoiseGate.h
+++ NoiseGate.h
@@ -5,6 +5,18 @@
 #ifndef NOISEGATE_H
 #define NOISEGATE_H
 
+class NoiseGate{
+	int noiseGap;
+public:
+	NoiseGate();
+	NoiseGate(int noiseGap);
+	virtual ~NoiseGate();
+	template<typename mono>
+	void processBuffer(mono buffer, int bufferSize);
+
+	template<typename stereo>
+	void processBuffer(stereo rightBuffer, stereo leftBuffer,  int bufferSize);
+};
 
 
 #endif
