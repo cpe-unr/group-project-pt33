9af90dc5488383a3df5353aea2bb39d78aab9b52
Mark Marsala <markinreno2001@gmail.com>
2021-05-02 23:36:14 -0700

    all mono sterio files compile

--- mono16.cpp
+++ mono16.cpp
@@ -1,3 +1,50 @@
-//Created by: Mark Marsala and Marshall Farris
+//Created by: Mark Marsala, Marshall Farris, and Johnathon Franco Sosa
 //Date: 5/4/2021
 //Reading and writing for an 16 bit mono wav file
+
+#include <string>
+#include <fstream>
+#include <iostream>
+#include "mono16.h"
+
+using namespace std;
+
+void Mono16::readFile()
+{
+	std::ifstream file(fileName,std::ios::binary | std::ios::in);
+	if(file.is_open())
+	{
+		file.read((char*)&waveHeader, sizeof(wav_header));
+		buffer = new short[waveHeader.data_bytes];
+		file.read(reinterpret_cast<char *>(buffer), waveHeader.data_bytes);
+		file.close();
+		cout << "Reading 16 bit mono file" << endl;
+	}
+}
+
+short* Mono16::getBuffer()
+{
+	return buffer;
+}
+
+void Mono16::writeFile(const std::string &outFileName)
+{
+	std::ofstream outFile(outFileName, std::ios::out | std::ios::binary);
+	outFile.write((char*)&waveHeader,sizeof(wav_header));
+	outFile.write(reinterpret_cast<char*>(buffer), waveHeader.data_bytes);
+	outFile.close();
+	cout << "Writing 16 bit mono file" << endl;
+}
+
+Mono16::~Mono16()
+{
+	if(buffer != NULL)
+	{
+		delete[] buffer;
+	}
+}
+
+int Mono16::getBufferSize() const 
+{
+	return waveHeader.data_bytes;
+}
--- stereo8.h
+++ stereo8.h
@@ -1,10 +1,28 @@
-//Created by: Mark Marsala and Marshall Farris
+//Created by: Mark Marsala, Marshall Farris, and Johnathon Franco Sosa
 //Date: 5/4/2021
 //Header file for stereo8.cpp
 
 #ifndef STEREO8_H
 #define STEREO8_H
+#include "waveHeader.h"
+#include "iFileIO.h"
 
-
-
+class Stereo8 : public iFileIO
+{
+	std::string fileName;
+	unsigned char* buffer = NULL;
+	wav_header waveHeader;
+	unsigned char* leftBuffer = NULL;
+	unsigned char* rightBuffer = NULL;
+public:
+	void readFile() override;
+	void writeFile(const std::string &outFileName) override;
+	unsigned char* getBuffer();
+	int getBufferSize() const;
+	void allocateBuffer();
+	void reallocateBuffer();
+	unsigned char* getLeftBuffer();
+	unsigned char* getRightBuffer();
+	virtual ~Stereo8();
+};
 #endif
