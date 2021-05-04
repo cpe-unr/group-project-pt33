5783b595750af520b971e0b84f7c9728bbe77aaa
Mark Marsala <markinreno2001@gmail.com>
2021-05-03 21:12:03 -0700

    Start of technical info

---
+++ analyzeWav.cpp
@@ -0,0 +1,3 @@
+//Created by: Mark Marsala, Marshall Farris, Ethan Thai, and Johnathon Franco Sosa
+//Date: 5/4/2021
+//
---
+++ analyzeWav.h
@@ -0,0 +1,10 @@
+//Created by: Mark Marsala, Marshall Farris, and Johnathon Franco Sosa
+//Date: 5/4/2021
+//Header file for analyzeWav.cpp
+
+#ifndef ANALYZEWAV_H
+#define ANALYZEWAV_H
+
+
+
+#endif
