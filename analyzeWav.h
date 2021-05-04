6bbf8a307c65b08e9380378fc6f54869bfd7a644
Marshall-F42 <marshallfarris@nevada.unr.edu>
2021-05-03 21:29:17 -0700

    added methods and variables to analyzeWav class.

--- analyzeWav.h
+++ analyzeWav.h
@@ -5,6 +5,34 @@
 #ifndef ANALYZEWAV_H
 #define ANALYZEWAV_H
 
+#include <iostream>
+#include <string>
+#include <vector>
 
+class AnalyzeWav{
+	std::string fileIn;
+	int numChan = 0;
+	int bitSize = 0;
+	int PCM = 0;
+	int bitRate = 0;
+	int sampleRate = 0;
+
+	waveHeader waveHeader;
+
+public:
+	AnalyzeWav(const std::string fin);
+	
+	void getTechData();
+	int getChan();
+	int getNumBits();
+	int getFileType();
+	int getBitRate();
+	int getSampleRate();
+	std::string getPCM();
+	std::vector<std::string> displayTechData;
+	
+	friend std::ostream & operator<< (std::ostream &out, AnalyzeWav &analyze);
+	
+};
 
 #endif
